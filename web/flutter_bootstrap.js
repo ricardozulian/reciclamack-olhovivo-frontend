3f8e1c3e3e8623906060c26c52e70654da81ed25
Ricardo_Amaral <ricardo@idadox.com>
2026-08-08 09:51:34 -0300

    Fix Flutter web release cache invalidation

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,8 +1,26 @@
 {{flutter_build_config}}
 {{flutter_js}}
-_flutter.loader.load({
-  config: {
-    canvasKitBaseUrl: "canvaskit/",
-    fontFallbackBaseUrl: "assets/assets/fonts/fallback/",
-  },
-});
+
+(async () => {
+  if ("serviceWorker" in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+  }
+  if ("caches" in window) {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+  }
+
+  for (const build of _flutter.buildConfig.builds) {
+    if (build.mainJsPath) {
+      build.mainJsPath = build.mainJsPath + "?v=0.2.0";
+    }
+  }
+
+  await _flutter.loader.load({
+    config: {
+      canvasKitBaseUrl: "canvaskit/",
+      fontFallbackBaseUrl: "assets/assets/fonts/fallback/",
+    },
+  });
+})();
